7a3307bf14b0eb5edcf7c4b8223edbe989594c0d
fbullrich <frankicanada@gmail.com>
2018-09-23 18:35:36 -0700

    added image processing for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,7 +47,7 @@ class ArticlesController < ApplicationController
 
     private
         def article_params
-            params.require(:article).permit(:title, :text)
+            params.require(:article).permit(:title, :text, :image)
         end
 
 
